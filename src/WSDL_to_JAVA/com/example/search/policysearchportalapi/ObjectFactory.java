8c605671cbf89f560ce692715316bd541b949836
AnkitaKundu <Ankita.Kundu@Exavalu.com>
2020-04-14 12:13:09 +0530

    Created com file

---
+++ src/WSDL_to_JAVA/com/example/search/policysearchportalapi/ObjectFactory.java
@@ -0,0 +1,56 @@
+
+package com.example.search.policysearchportalapi;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the com.example.search.policysearchportalapi package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.example.search.policysearchportalapi
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link GetAccountInfo }
+     * 
+     */
+    public GetAccountInfo createGetAccountInfo() {
+        return new GetAccountInfo();
+    }
+
+    /**
+     * Create an instance of {@link GetAccountInfoResponse }
+     * 
+     */
+    public GetAccountInfoResponse createGetAccountInfoResponse() {
+        return new GetAccountInfoResponse();
+    }
+
+    /**
+     * Create an instance of {@link WsiAuthenticationException }
+     * 
+     */
+    public WsiAuthenticationException createWsiAuthenticationException() {
+        return new WsiAuthenticationException();
+    }
+
+}
