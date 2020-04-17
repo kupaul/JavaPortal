0e94863921f2a01e62ff8f3501906d173caf685e
DamjaPaulami <damjapaulami@gmail.com>
2020-04-17 17:23:04 +0530

    Git Push by DP on 17/04/2020 for Service Class

--- src/com/gw/serachPortal/Controller/LoginServlet.java
+++ src/com/gw/serachPortal/Controller/LoginServlet.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.guidewire.webservice.ExternalAPI;
 import com.gw.searchPortal.Util.DBUtil;
 
 
@@ -21,6 +23,8 @@ public class LoginServlet extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	Logger logger = Logger.getLogger(ExternalAPI.class.getName());
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String un=request.getParameter("username");
@@ -31,9 +35,8 @@ public class LoginServlet extends HttpServlet {
 		try {
 			
 		Connection	 conn =  DBUtil.createConnection();
- 
-		System.out.println("<<<<User name>>>> "+ un );
-		System.out.println("<<<Password>>> "+ pw );
+        logger.info("<<<<User name>>>> "+ un );
+        logger.info("<<<Password>>> "+ pw );
 		 
 		if(un !=null) {
 			 HttpSession session = request.getSession();
