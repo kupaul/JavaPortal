1dcf5e61f9b5ede352a19d1c2d6bb6c24abbf80f
debrupa96 <Debrupa.Datta@Exavalu.com>
2020-05-07 18:37:32 +0530

    com file created

---
+++ src/contact_info_com_file/com/example/search/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://example.com/search", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.example.search;
---
+++ src/contact_info_com_file/com/gw/searchPortal/Controller/LoginServlet.java
@@ -0,0 +1,73 @@
+package com.gw.searchPortal.Controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+import com.gw.searchPortal.Util.DBUtil;
+
+
+public class LoginServlet extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	Logger logger = Logger.getLogger(LoginServlet.class.getName());
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String un=request.getParameter("username");
+		String pw=request.getParameter("pass");
+		
+	
+		
+		try {
+			
+		Connection	 conn =  DBUtil.createConnection();
+        logger.info("<<<<User name>>>> "+ un );
+        logger.info("<<<Password>>> "+ pw );
+		 
+		if(un !=null) {
+			 HttpSession session = request.getSession();
+			 session.setAttribute("userName", un);
+		}
+		
+		PreparedStatement ps = conn.prepareStatement("select userName,password from userInfo where username=? and password=?");
+		ps.setString(1, un);
+		ps.setString(2, pw);
+ 
+		ResultSet rs = ps.executeQuery();
+	
+		while (rs.next()) {
+			//System.out.println("----For Home.jsp--");
+			response.sendRedirect("Home.jsp");
+			return;
+		}
+		
+		//System.out.println("----For Error.jsp----");
+		response.sendRedirect("Error.jsp");
+		return;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
+
+
+
+
+
+
